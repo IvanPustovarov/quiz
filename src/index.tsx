c7b1fb7700c1291b1ccf00514cc310956a42e24a
winston <ivanpustovarov5@gmail.com>
2023-09-20 16:09:24 +0300

    provire redux stote

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '~/app';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
 const anchor = document.getElementById('root');
+
 if (anchor) {
-  createRoot(anchor).render(<App />);
+  createRoot(anchor).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 }
